1392760d553632ad56e3466b4704566fa3dc948d
murali <murali>
2023-03-23 07:19:43 +0000

    gcs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,54 @@
-output "id" {
-  description = "Repository id"
-  value       = google_artifact_registry_repository.registry.id
+output "bucket" {
+  description = "Bucket resource (for single use)."
+  value       = local.first_bucket
 }
 
 output "name" {
-  description = "Repository name"
-  value       = google_artifact_registry_repository.registry.name
+  description = "Bucket name (for single use)."
+  value       = local.first_bucket.name
+}
+
+output "url" {
+  description = "Bucket URL (for single use)."
+  value       = local.first_bucket.url
+}
+
+output "buckets" {
+  description = "Bucket resources as list."
+  value       = local.buckets_list
+}
+
+output "buckets_map" {
+  description = "Bucket resources by name."
+  value       = google_storage_bucket.buckets
+}
+
+output "names" {
+  description = "Bucket names."
+  value = { for name, bucket in google_storage_bucket.buckets :
+    name => bucket.name
+  }
+}
+
+output "urls" {
+  description = "Bucket URLs."
+  value = { for name, bucket in google_storage_bucket.buckets :
+    name => bucket.url
+  }
+}
+
+output "names_list" {
+  description = "List of bucket names."
+  value       = local.buckets_list[*].name
+}
+
+output "urls_list" {
+  description = "List of bucket URLs."
+  value       = local.buckets_list[*].url
+}
+
+output "hmac_keys" {
+  description = "List of HMAC keys."
+  value       = google_storage_hmac_key.hmac_keys[*]
+  sensitive   = true
 }
