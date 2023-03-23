5ec28a2c8cfcc6f03236e36122be67c085bed60a
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2023-03-23 12:51:20 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,3 +1,14 @@
 terraform {
-  required_version = ">= 0.12.6"
-}
+  required_version = ">= 0.13"
+  required_providers {
+
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.42, < 5.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.1"
+    }
+  }
